814c4893c2f8bb1fe0d8340286e6fc6232c3b742
Kalmyrza <Zholchubekovk@gmail.com>
2022-02-22 15:29:13 +0600

    useState class project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [stars, setStars] = useState([0,1,2,3,4]);
+ const [rate, setRate] = useState(localStorage.getItem('rate'));
+
+ const handlerClick = (index) => {
+  setRate(index);
+  localStorage.setItem('rate', index)
+ }
+
+ const removeFromLocalStorage = () => {
+   localStorage.removeItem('rate')
+   setRate(0)
+ }
+ let paragraph = <p>:D</p>
+ return (
+   <>
+   <h1>Hello World</h1>
+   {stars.map((star, index) => {
+     return (
+      <div 
+      style={
+        {
+          display: 'inline-block',
+          border: '1px solid black',
+          padding: '10px'
+
+        }
+      }
+      onMouseOver={() => handlerClick(index)}
+
+      className={index <= rate ? 'fill' : ''}
+      > 
+      {star}
+      </div>
+     )
+   })}
+   <button onClick={() => removeFromLocalStorage()}
+   style={
+     {
+       position: 'relative',
+       bottom: '10px',
+       width: '100px',
+       height: '40px',
+       margin: '10px'
+     }
+   }
+   >Remove from local storage</button>
+   <p>{parseInt(rate)+1}/5</p>
+   {parseInt(rate) === 4 ? paragraph : null}
+   </>
+ )
 }
 
 export default App;
